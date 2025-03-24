4157a6cb88c243a4219aef72a84d05408cbed1e7
hoed <hoedhud@gmail.com>
2025-03-24 07:20:29 +0700

    update

--- src/components/auth/auth-form-header.tsx
+++ src/components/auth/auth-form-header.tsx
@@ -11,7 +11,7 @@ const AuthFormHeader = ({ isSignUp }: AuthFormHeaderProps) => {
       <h2 className="text-3xl font-bold text-blue bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-purple-900">
         {isSignUp ? 'Create an Account' : 'Welcome Back'}
       </h2>
-      <p className="text-gray-400 mt-2">
+      <p className="text-blue-700 mt-2">
         {isSignUp
           ? 'Sign up to start planning amazing tours'
           : 'Log in to access your dashboard'}
