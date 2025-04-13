d7c637ae0236086ea751df0e68264ec6299f3acf
hoedhud <hoedhud@gmail.com>
2025-04-13 23:58:19 +0700

    Update Navbar

--- src/components/landing/navbar.tsx
+++ src/components/landing/navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
         isScrolled
           ? 'py-3 glass shadow-md'
-          : 'py-6 bg-transparent'
+          : 'py-9 bg-transparent'
       }`}
     >
       <div className="container mx-auto px-6 flex items-center justify-between">
